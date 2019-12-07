5b96e990469036849303b1da30fd0c141b4f1202
TakeAsh <take@TakeAsh.net>
2019-12-07 23:31:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+DBIx::NamedParams - It's new $module
+
+# SYNOPSIS
+
+    use DBIx::NamedParams;
+
+# DESCRIPTION
+
+DBIx::NamedParams is ...
+
+# LICENSE
+
+Copyright (C) TakeAsh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+TakeAsh &lt;take@TakeAsh.net>
---
+++ lib/DBIx/NamedParams.pm
@@ -0,0 +1,39 @@
+package DBIx::NamedParams;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::NamedParams - It's new $module
+
+=head1 SYNOPSIS
+
+    use DBIx::NamedParams;
+
+=head1 DESCRIPTION
+
+DBIx::NamedParams is ...
+
+=head1 LICENSE
+
+Copyright (C) TakeAsh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+TakeAsh E<lt>take@TakeAsh.netE<gt>
+
+=cut
+
